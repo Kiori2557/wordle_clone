222742fec77828dea13ad86ba25cec5313a0fb37
Aung Khant Paing <akpaing99@gmail.com>
2025-04-18 21:55:34 +0630

    save and render user guesses

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,34 +1,57 @@
 import useStore from "../store/store";
 import Row from "./Row";
 import { useState, useEffect, useCallback } from "react";
-
 function Board() {
   const chance = useStore((state) => state.chance);
-  const [guess, setGuess] = useState<string>("");
+  const allGuesses = useStore((state) => state.allGuesses);
+  const [guesses, setGuesses] = useState<string[]>([""]);
   const answerLength = useStore((state) => state.answerLength);
-  const numOfGuess: number = useStore((state) => state.allGuesses.length);
-  const keyPressHandler = useCallback(
+  const answer = useStore((state) => state.answer);
+  const { addGuess } = useStore.getState();
+  const keyDownHandler = useCallback(
     (e: KeyboardEvent) => {
-      setGuess((guess: string) => {
-        if (answerLength - 1 > 4) return guess;
-        const updated = guess + e.key;
-        console.log(updated.length);
-        return updated;
+      if (e.repeat) return;
+      const currentGuess = guesses[guesses?.length - 1];
+      if (e.key === "Enter") {
+        if (currentGuess === answer) {
+          console.log("u win");
+        } else {
+          addGuess(currentGuess);
+          setGuesses([...guesses, ""]);
+        }
+        console.log(allGuesses);
+        return;
+      }
+
+      setGuesses((guesses: string[]) => {
+        const lastGuess = guesses[guesses.length - 1];
+        if (e.key === "Backspace") {
+          console.log("delete");
+          return [...allGuesses, lastGuess.slice(0, lastGuess.length - 1)];
+        } else if (
+          lastGuess.length < answerLength &&
+          /^[a-zA-Z]$/.test(e.key)
+        ) {
+          const updated = lastGuess + e.key;
+          return [...allGuesses, updated];
+        } else {
+          return [...allGuesses, lastGuess];
+        }
       });
     },
-    [answerLength]
+    [answerLength, answer, addGuess, guesses, allGuesses]
   );
   useEffect(() => {
-    window.addEventListener("keypress", keyPressHandler);
+    window.addEventListener("keydown", keyDownHandler);
     console.log("useEffect run");
     return () => {
-      window.removeEventListener("keypress", keyPressHandler);
+      window.removeEventListener("keydown", keyDownHandler);
     };
-  }, [keyPressHandler]);
+  }, [keyDownHandler]);
   return (
     <div className="flex flex-col justify-center place-self-center gap-1">
       {[...Array(chance)].map((_, i) => {
-        return <Row key={i} guess={i === numOfGuess ? guess : ""} />;
+        return <Row key={i} guess={guesses[i]} />;
       })}
     </div>
   );
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,7 +3,9 @@ type TileProps = {
 };
 function Tile({ letter }: TileProps) {
   return (
-    <div className=" w-20 h-20 border-2 border-black rounded-sm">{letter}</div>
+    <div className=" w-20 h-20 border-2 border-black rounded-sm inline-flex items-center justify-center text-4xl align-middle uppercase leading-none font-bold">
+      {letter}
+    </div>
   );
 }
 export default Tile;
